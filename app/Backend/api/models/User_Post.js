c13aad10a1eb1be9f753f24e2264284b05ae4161
MrHmdavid <94898454+MrHmdavid@users.noreply.github.com>
2025-11-09 09:24:54 +0100

    Updated Post

--- app/Backend/api/models/User_Post.js
+++ app/Backend/api/models/User_Post.js
@@ -46,8 +46,12 @@ module.exports = (sequelize, DataTypes) =>
                 type: DataTypes.STRING(1000),
                 allowNull: false,
             },
+            title:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
 
-            has_media:
+            media_url:
             {
                 type: DataTypes.BOOLEAN,
                 allowNull: false,
@@ -76,7 +80,7 @@ module.exports = (sequelize, DataTypes) =>
             updatedAt: "updated_at", 
             scopes: {
                 allPostData:{
-                    attributes: ["ID", "USER_ID", "like", "dislike", "visibility", "has_media", "content", "created_at", "updated_at"],
+                    attributes: ["ID", "USER_ID", "like", "dislike", "visibility", "media_url", "content", "created_at", "updated_at","title"],
                 }
             },
         }
