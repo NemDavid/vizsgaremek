3881ec41aaab6887e446e33e51fbbcb7bc9a27f7
NemDavid <hartwig-matos@ckik.hu>
2025-11-10 14:05:09 +0100

    ads

--- app/Frontend/src/components/AdsFrame.tsx
+++ app/Frontend/src/components/AdsFrame.tsx
@@ -1,9 +1,9 @@
-export function AdsFrame(){
+export function AdsFrame({url}:{url:string}){
     return (
-        <aside className="w-[300px] border-r-2 border-gray-300 p-4 bg-gray-100">
+        <aside className="w-[300px] border-r-2 border-l-2 border-gray-300 p-4 bg-gray-100">
                 <h2 className="font-bold text-lg mb-2">Hirdetés</h2>
                 <img
-                    src="/hirdetes.png"
+                    src={url}
                     alt="Hirdetés"
                     className="w-full h-auto rounded"
                     />
--- app/Frontend/src/components/DefaultUIFrame.tsx
+++ app/Frontend/src/components/DefaultUIFrame.tsx
@@ -1,23 +1,42 @@
+import axios from "axios";
 import { AdsFrame } from "./AdsFrame";
 import { Footer } from "./footer";
 import Header from "./Header";
 import { PostsFrame } from "./PostsFrame";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner } from "./ui/spinner";
 
 interface DefaultUIFrameProps {
   children?: React.ReactNode; // Ide jöhet majd az, amit a "ide" helyére akarsz rakni
 }
 
+export async function fetchAds() {
+  return await axios("https://api.example-ads.com/ads?apikey=YOUR_KEY");
+}
+
 export function DefaultUIFrame({ children }: DefaultUIFrameProps){
+    // const {data: ads, isLoading} = useQuery({
+    //     queryKey: ['Ads'],
+    //     queryFn: () => fetchAds(),
+    //     retry: 0,
+    //     refetchOnWindowFocus: false,
+    // })
+
+    // if(isLoading){
+    //     return <Spinner/>
+    // }
     return (
         <>
-            <div className="h-screen">
+            <div className="flex flex-col h-screen">
                 <Header/>
-                <div className="flex flex-col bg-white text-black">
+                <div className="flex flex-1 bg-white text-black">
                     {/* <FriendList/> */}
-                    <div className="flex flex-1">
-                        <AdsFrame/>
+                    <div className="flex flex-1 ">
+                        <AdsFrame url={"/hirdetes.png"} />
                         {children}
                         <PostsFrame/>
+                        <AdsFrame url={"/hirdetes2.png"} />
+
                     </div>
                 </div>
             </div>
