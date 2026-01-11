009345283f4a9f9f041097f28e206a29bf156107
Murar.Balint <murar.balint@ckik.hu>
2026-01-11 22:23:41 +0100

    faszom docker izé megcsinélva hála Istennek

--- app/Backend/api/utilities/cloudUtils.js
+++ app/Backend/api/utilities/cloudUtils.js
@@ -1,37 +1,37 @@
-const uuidv4  = require("uuid").v4
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const crypto = require("crypto");
 const { ValidationError } = require("../errors");
 
+exports.getStorage = () => {
+  const uploadDir = path.join(__dirname, "../../public/cloud");
 
-exports.getStorage = ( )=>{
-    const uploadDir = "./public/cloud";
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
+  
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+      const uniqueName =
+        crypto.randomUUID() + path.extname(file.originalname);
+      cb(null, uniqueName);
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp", "image/jpg"];
+
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new ValidationError("Rossz file típus"), false);
     }
-    
-    const storage = multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, uploadDir);
-      },
-      filename: (req, file, cb) => {
-        const uniqueName = uuidv4() + "." + file.originalname.split('.').pop();
-        cb(null, uniqueName);
-      },
-    });
-    
-    const fileFilter = (req, file, cb) => {
-      const allowedTypes = ["image/jpeg", "image/png", "image/webp", "image/jpg"];
-    
-      if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-      } else {
-        cb(new ValidationError("Rosz file tipus -> .jpeg | .png | .webp | .jpg"), false); // 
-      }
-    };
-    
-    const upload = multer({ storage, fileFilter });
-    
-    return upload
-}
+  };
+
+  return multer({ storage, fileFilter });
+};
