b8c61715c443e5488092dfb432fb749622077878
NemDavid <hartwig-matos@ckik.hu>
2026-01-23 13:11:03 +0100

    bug fix

--- app/Backend/server.js
+++ app/Backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config({ quiet: true })
 const app = require("./app")
 
 const PORT = process.env.PORT || 6769
-const HOST = process.env.Docker_Active != "false" ? "0.0.0.0":"localhost"
+const HOST = "0.0.0.0"
 
 app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`)
